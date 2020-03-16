024474d23359a76e428a860b5b55711cc9b0b89c
akhil <akhilddn5@gmail.com>
2020-03-16 17:43:03 +0100

    add comments for the script

--- SZ_runAll.m
+++ SZ_runAll.m
@@ -1,23 +1,31 @@
-% Script to run the pRF analysis after running pRF models (2D Gaussian and DoG) 
-% Models can be run using the script SZ_mrV.m
+% Main script to run the pRF analysis after running pRF models (2D Gaussian and DoG) using the script SZ_mrV.m
 
-clear all; close all;
+% Run SZ_runAll.m from folder containing the folder named "mrvista" which in turn
+% contain the folders with subject names and contain vistasession for that subject.  
+
+% Use SZ_getOpts.m function to set different options such as modelType,
+% plotType. 
+
+% SZ_pRFAnalysis.m will extract the pRF model parameters as set by the
+% plot_Type and save the relevant figures. 
+
+% SZ_statsRunAll.m can be used to run the statistical analysis (an n-way anova for every ROI separately). 
+
+%%
 % 1. sigma vs eccentricity
 modelType = '2DGaussian';
 plotType = 'Ecc_Sig';
 opt = SZ_getOpts('modelType',modelType,'plotType',plotType,'verbose',1,'extractPrfParams',0,'detailedPlot',0,'getTimeSeries',1,'getPredictedResponse',1,'recomputeTimeSeries',0);
 SZ_pRFAnalysis(opt);
 close all;
-
 %%
-
 % 2. fwhm vs eccentricity (DoGs)
 clear all; close all;
 modelType = 'DoGs';
 plotType = 'Ecc_Sig_fwhm_DoGs';
 opt = SZ_getOpts('modelType',modelType,'plotType',plotType,'verbose',1,'extractPrfParams',0,'detailedPlot',0,'getTimeSeries',0,'getPredictedResponse',0,'recomputeTimeSeries',0);
 SZ_pRFAnalysis(opt);
-
+%%
 % 3. surround size vs eccentricity (DoGs)
 clear all; close all;
 modelType = 'DoGs';
@@ -36,7 +44,6 @@ SZ_pRFAnalysis(opt);
 close all;
 
 %%
-
 % 3. sigma1 vs eccentricity (DoGs)
 clear all; close all;
 modelType = 'DoGs';
@@ -44,8 +51,7 @@ plotType = 'Ecc_Sig1_DoGs';
 opt = SZ_getOpts('modelType',modelType,'plotType',plotType,'verbose',1,'extractPrfParams',0,'getTimeSeries',0,'detailedPlot',0','recomputeTimeSeries',0);
 SZ_pRFAnalysis(opt);
 close all;
-
-
+%%
 % 3. suppression index vs eccentricity (DoGs)
 clear all; close all;
 modelType = 'DoGs';
@@ -53,7 +59,7 @@ plotType = 'Ecc_SuppressionIndex_DoGs';
 opt = SZ_getOpts('modelType',modelType,'plotType',plotType,'verbose',1,'extractPrfParams',0,'getTimeSeries',0,'detailedPlot',0','recomputeTimeSeries',0);
 SZ_pRFAnalysis(opt);
 close all;
-
+%%
 % 3. sigma ratio vs eccentricity (DoGs)
 clear all; close all;
 modelType = 'DoGs';
--- stats/SZ_statsRunAll.m
+++ stats/SZ_statsRunAll.m
@@ -1,3 +1,23 @@
+% SZ_statsRunAll to perform statistical analysis on either the central
+% values or area under curve values extracted from all subjects from the
+% different subject groups.
+%
+% opt.compare can be chosed appropriately to decide if the schizophrenia
+% patients should be considered together as one patient group, as two
+% different groups when comparing with the healthy controls. Also, there is
+% an option to compate only the patinet groups without the healthy
+% controls. 
+%
+% opt.roisToCompare can be set to define the ROIS to compare
+%
+% opt.paramToCompare can take as input either 'central' or 'AUC'
+%
+% curParam is used to define the parameter that is compared
+% 
+% modelType is used to define the tye of model (either 1D gaussian or Difference of gaussians)
+%
+% For different options check SZ_getOpts.m function
+
 clear all;
 opt.compare = 'patientsTogether';
 %opt.compare = 'patientsSeparate';
@@ -9,14 +29,11 @@ opt.roisToCompare = 'V1234';
 opt.paramToCompare = 'central';
 
 %%
-
 curParam = 'sigma_1G';
 modelType = '2DGaussian';
 plotType = 'Ecc_Sig';
 opt = SZ_getOpts('modelType',modelType,'plotType',plotType,'verbose',1,'compare',opt.compare,'roisToCompare',opt.roisToCompare,'paramToCompare',opt.paramToCompare);
 fprintf('%s',curParam)
-
-
 
 % SZ_repeatedMeasuresAnova(curParam,opt);
 % SZ_unpairedTtest(curParam,opt);
